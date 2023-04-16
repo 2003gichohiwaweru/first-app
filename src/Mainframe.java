4342bf9d0f4a94bee358a67400074816403fe012
JosephGichohi <josephgichohi@gmail.com>
2023-04-16 21:59:16 +0300

    first commit

---
+++ src/Mainframe.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Mainframe extends JFrame{
+    public void init (){
+        setTitle("Welcome");
+        setSize(400, 240);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    
+}
